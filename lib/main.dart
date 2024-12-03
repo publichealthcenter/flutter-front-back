9871019c960dc6dde75c3da50f5ee22306404fe4
mstagon <devcms21@gmail.com>
2024-12-04 01:29:21 +0900

    add router

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/backoffice.dart';
+import 'package:untitled/page/floorinfo.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
+  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Backoffice(),
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        print('Current route: ${settings.name}');
+        switch (settings.name) {
+          case '/acceptance':
+            return MaterialPageRoute(builder: (context) => Acceptance());
+          case '/backoffice':
+            return MaterialPageRoute(builder: (context) => Backoffice());
+          case '/floorinfo':
+            return MaterialPageRoute(builder: (context) => FloorInfo());
+          default:
+            return MaterialPageRoute(builder: (context) => Backoffice());
+        }
+      },
     );
   }
 }
